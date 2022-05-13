42bad767ef3ce89307e1de5881b27da4c3b3e7fa
WutongCoCo <wutongcoco@gmail.com>
2022-05-13 16:33:57 +0800

    modified: views.py

--- myDjango/urls.py
+++ myDjango/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myApp.views import sayhello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sayhello/', sayhello),
 ]
+ 
