349c7f4c04a87af38d1e15109ae686ec62c5ae06
WutongCoCo <wutongcoco@gmail.com>
2022-06-10 17:11:41 +0800

    commit

--- myDjango/settings.py
+++ myDjango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+)oss^af_1dm=3ry9tgd4gfu*-@t7$rlxwqi95sa^w518-djf5'
+SECRET_KEY = 'django-insecure-)4qjw(36rtcx-gpo$vb^y=6b50ylqv9rehl)qag5+3=&i_9n(c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR/'static',
+    BASE_DIR / 'static',
 ]
 
 # Default primary key field type
