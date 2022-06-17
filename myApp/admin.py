96d15344d9fef1e49a84b22c6bc322543d4fdaf2
WutongCoCo <wutongcoco@gmail.com>
2022-06-17 16:41:59 +0800

    0617

--- myApp/admin.py
+++ myApp/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from myApp.models import student
 
 # Register your models here.
-admin.site.register(student)
+
+
+class studentAdmin(admin.ModelAdmin):
+    list_display = ('id','cName','cSex','cBirthday','cEmail','cPhone','cAddr')
+    
+admin.site.register(student,studentAdmin)
